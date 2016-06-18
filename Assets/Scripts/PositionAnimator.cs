2974c977652b24c725e5f25e15ec9a2f01d94ebf
cbleck <carlos@capptu.com>
2016-06-17 19:37:34 -0500

    1st commit

---
+++ Assets/Scripts/PositionAnimator.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PositionAnimator : MonoBehaviour {
+
+    public Vector3 startPosition;
+    public Vector3 endPosition;
+
+    public float duration;
+    private float startTime;
+    private bool animating = false;
+
+    // Update is called once per frame
+    void Update() {
+
+        if (duration != 0) { 
+
+            if (Input.GetKeyDown(KeyCode.Space) && !animating) {
+
+                startTime = Time.time;
+                animating = true;
+            }
+
+            if (animating) {
+
+                transform.position = Vector3.Lerp(startPosition, endPosition, (Time.time - startTime) / duration);
+
+                if (Time.time - startTime > duration) {
+                    animating = false;
+                }
+            }
+        }
+	}
+}
